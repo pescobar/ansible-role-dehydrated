3495ba295608919b429e68220647f68c46e62c0b
Pablo Escobar <pablo.escobarlopez@unibas.ch>
2019-06-17 17:27:14 +0200

    added some basic unit tests

--- molecule/tests/test_default.py
+++ molecule/tests/test_default.py
@@ -6,9 +6,29 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
+def test_dehydrated_config(host):
+    f = host.file('/etc/dehydrated/config')
     assert f.exists
+    assert f.is_file
     assert f.user == 'root'
     assert f.group == 'root'
+    assert oct(f.mode) == '0644'
+    assert f.contains('CONTACT_EMAIL')
+
+
+def test_dehydrated_domains_file(host):
+    f = host.file('/etc/dehydrated/domains.txt')
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert oct(f.mode) == '0644'
+
+
+def test_dehydrated_script(host):
+    f = host.file('/usr/local/bin/dehydrated')
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert oct(f.mode) == '0755'
